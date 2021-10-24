6d7aca77db00908fdea0900b94060d5fff2ac91f
Ulrik Hørlyk Hjort <Ulrik.Hoerlyk.Hjort@gmail.com>
2021-10-24 21:53:51 +0200

    Update midi2arduino.py

--- midi2arduino.py
+++ midi2arduino.py
@@ -8,11 +8,11 @@
 #                                                                           
 #                 Copyright (C) 2019 Ulrik Hoerlyk Hjort                   
 #                                                                        
-#  Ipod file extract is free software;  you can  redistribute it                          
+#  Midi to Arduino is free software;  you can  redistribute it                          
 #  and/or modify it under terms of the  GNU General Public License          
 #  as published  by the Free Software  Foundation;  either version 2,       
 #  or (at your option) any later version.                                   
-#  Ipod file extract is distributed in the hope that it will be                           
+#  Midi to Arduino is distributed in the hope that it will be                           
 #  useful, but WITHOUT ANY WARRANTY;  without even the  implied warranty    
 #  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  
 #  See the GNU General Public License for  more details.                    
